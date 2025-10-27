2dbd02c957168bb3faf3f837c599177aad7cd6fc
Avinesh sharma <142591660+avineshsharma@users.noreply.github.com>
2025-10-27 23:41:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	const newstr = new Map();
+	for (let  char of str) {
+		newstr.set(char, (newstr.get(char)||0)+1);
+	}
+	for(let char of str){
+		if(newstr.get(char) ===1){
+			return char;
+		}
+	}
+	return null;
+	
 }
-const input = prompt("Enter a string");
+// const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
